842acd85dee68426196a3e2d6eba3296bb5ef240
Yashwanth Maringanti <yashwanthmaringanti@gmail.com>
2026-01-09 18:54:20 +0530

    pizza next application with product card, topping card and topping list ts files updated and assets

---
+++ lib/providers.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { CartProvider } from './cart-context';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <CartProvider>
+            {children}
+        </CartProvider>
+    );
+}
